b676f0618edf1187d986adee1bb56bdf5ec5053f
Dharrent Qurratuain <dharrentq5@gmail.com>
2026-04-04 21:39:07 +0700

    Update no2.go

--- Week 5 - Modul 5/Unguided/Unguided 2/no2.go
+++ Week 5 - Modul 5/Unguided/Unguided 2/no2.go
@@ -1,1 +1,21 @@
+package main
+import "fmt"
+5
+func faktor(n, i int) {
+    if i > n {
+        return
+    }
+    if n%i == 0 {
+        fmt.Print(i, " ")
+    }
+    faktor(n, i+1)
+}
 
+func main() {
+    var n int
+    fmt.Print("Masukkan bilangan N: ")
+    fmt.Scan(&n)
+
+    fmt.Println("Faktor dari", n, ":")
+    faktor(n, 1)
+}
