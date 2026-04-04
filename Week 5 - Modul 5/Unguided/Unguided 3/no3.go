bcd56d834740b5a5676af5625a7a863792bfe192
Dharrent Qurratuain <dharrentq5@gmail.com>
2026-04-04 21:39:30 +0700

    Update no3.go

--- Week 5 - Modul 5/Unguided/Unguided 3/no3.go
+++ Week 5 - Modul 5/Unguided/Unguided 3/no3.go
@@ -1,1 +1,19 @@
+package main
+import "fmt"
 
+func ganjil(n, i int) {
+    if i > n {
+        return
+    }
+    fmt.Print(i, " ")
+    ganjil(n, i+2)
+}
+
+func main() {
+    var n int
+    fmt.Print("Masukkan bilangan N: ")
+    fmt.Scan(&n)
+
+    fmt.Println("Bilangan ganjil dari 1 sampai", n, ":")
+    ganjil(n, 1)
+}
